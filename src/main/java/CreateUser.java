b0fe8a8448069b2e067e6ee39b5f10abe5d13591
hannahj001 <hannahjohnstongk@gmail.com>
2022-10-18 19:19:39 -0400

    CreateUserClass

--- src/main/java/CreateUser.java
+++ src/main/java/CreateUser.java
@@ -1,2 +1,36 @@
 public class CreateUser {
+    String name;
+    String email;
+    String phoneNumber;
+
+    public CreateUser(String name, String email, String phoneNumber){
+        this.email = email;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
