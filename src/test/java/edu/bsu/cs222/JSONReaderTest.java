a652b7e9bcb23424e7bf6d94dc90f154f42f1327
ed749 <ed74666@gmail.com>
2022-10-19 11:24:26 -0400

    Added to JSONReaderTest

--- src/test/java/edu/bsu/cs222/JSONReaderTest.java
+++ src/test/java/edu/bsu/cs222/JSONReaderTest.java
@@ -7,19 +7,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 
 public class JSONReaderTest {
     @Test
-    public void readName() throws FileNotFoundException, ParseException {
+    public void readName() throws FileNotFoundException, ParseException, URISyntaxException {
         JSONParser jsonParser = new JSONParser();
 
 
-        //URL resource = getClass().getClassLoader().getResource("User1.json");
-
-        FileReader reader = new FileReader("/Users/erika/Final-megan237-ed749-hannahj001-ceendris/src/test/resources/User1.json");
+        URL resource = getClass().getClassLoader().getResource("User1.json");
+        FileReader reader = new FileReader(new File(resource.toURI()));
+        //FileReader reader = new FileReader("/Users/erika/Final-megan237-ed749-hannahj001-ceendris/src/test/resources/User1.json");
         Object object = jsonParser.parse(reader);
         JSONReader JSONFileReader = new JSONReader();
         String result = JSONFileReader.parseName(object);
